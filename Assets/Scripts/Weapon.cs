31bc024de38e262bb975db39fb5251f135f3d0e3
mancu37 <mancu37@gmail.com>
2017-09-11 21:35:04 -0300

    Added Stuff
    Added Stuff

---
+++ Assets/Scripts/Weapon.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using UnityEngine;
+
+[Serializable]
+public class Weapon
+{
+    public string Name;
+    
+    public enum TypeEnum {
+        Automatic, Manual, sniper
+    };
+    public TypeEnum Type;
+    public int Amount;
+    public int Damage;
+    public int Distance;
+    public float WeaponNextFireRate;
+    public float WeaponFireRate;
+    public ParticleSystem Muzzeflash;
+
+}
---
+++ Assets/Scripts/WeaponManager.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponManager : MonoBehaviour {
+
+    public int selectedWeapon = 0;
+    public int previousSelectedWeapon;
+
+    public Animator animator;
+    public float weaponNextFireRate = 0f;
+    public float weaponFireRate = 15f;
+    public ParticleSystem muzzeflash;
+
+    [SerializeField]
+    public Weapon[] weapons;
+
+    // Use this for initialization
+    void Start () {
+        SelectWeapon();
+	}
+
+    private void SelectWeapon()
+    {
+        int i = 0;
+        foreach (Transform weapon in transform)
+        {
+            if (i == selectedWeapon)
+            {
+                weapon.gameObject.SetActive(true);
+                animator = weapon.GetComponent<Animator>();
+            }
+            else
+            {
+                weapon.gameObject.SetActive(false);
+            }
+
+            i++;
+        }
+    }
+
+    // Update is called once per frame
+    void Update () {
+        if (Input.GetButton("Fire2"))
+        {
+            
+            animator.SetBool("Aim", true);
+        }
+        else
+        {
+            animator.SetBool("Aim", false);
+        }
+
+        if (Input.GetButton("Fire1") && weapons[selectedWeapon].Type == Weapon.TypeEnum.Automatic)
+        {
+
+            animator.SetBool("Shoot", true);
+
+            if (Time.time >= weapons[selectedWeapon].WeaponNextFireRate)
+            {
+                
+
+                weapons[selectedWeapon].WeaponNextFireRate = Time.time + 1f / weapons[selectedWeapon].WeaponFireRate;
+
+                weapons[selectedWeapon].Muzzeflash.Play();
+
+                
+            }
+        }
+
+        if (Input.GetButtonDown("Fire1") && weapons[selectedWeapon].Type == Weapon.TypeEnum.Manual)
+        {
+
+            
+
+            if (Time.time >= weapons[selectedWeapon].WeaponNextFireRate)
+            {
+                animator.SetBool("Shoot", true);
+
+                weapons[selectedWeapon].WeaponNextFireRate = Time.time + weapons[selectedWeapon].WeaponFireRate;
+
+                weapons[selectedWeapon].Muzzeflash.Play();
+
+
+            }
+        }
+
+        if (!Input.GetButton("Fire1") || !Input.GetButtonDown("Fire1"))
+        {
+            animator.SetBool("Shoot", false);
+        }
+
+
+        previousSelectedWeapon = selectedWeapon;
+
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            selectedWeapon = 0;
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            selectedWeapon = 1;
+        }
+
+        if (previousSelectedWeapon != selectedWeapon)
+            SelectWeapon();
+    }
+}
